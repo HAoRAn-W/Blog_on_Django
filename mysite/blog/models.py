7803748ec72bbe7b2974add637881655008c28ff
haoran <1348195048@qq.com>
2020-06-17 20:13:46 +0800

    Initial commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    # blog category
+    name = models.CharField('name', max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    # blog tag
+    name = models.CharField('name', max_length=16)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField('title', max_length=32)  # 标题
+    author = models.CharField('author', max_length=16)  # 标题
+    content = models.CharField('content', max_length=2048)  # 正文
+    publish_time = models.DateTimeField('publish_time', auto_now_add=True)  # 发布时间
+    category = models.CharField(Category, max_length=30)  # 分类
+    tags = models.ManyToManyField(Tag)  # 标签
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Article, on_delete=models.CASCADE)
+    visitor_name = models.CharField('visitor_name', max_length=16)
+    email = models.EmailField('email')
+    comments = models.CharField('comments', max_length=480)
+    publish_time = models.DateTimeField('publish_time', auto_now_add=True)
+
+    def __str__(self):
+        return self.comments
