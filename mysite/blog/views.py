cb9cf3fd1e4ecb414d002e96d7b5aa3d334d25da
haoran <1348195048@qq.com>
2020-06-20 10:48:34 +0800

    code block highlight

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .comment import *
 from .models import *
+import markdown
 
 
 def index(request):
@@ -10,5 +11,8 @@ def index(request):
 
 def detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
+    blog.content = markdown.markdown(blog.content, extensions=['markdown.extensions.extra',
+                                                               'markdown.extensions.codehilite',
+                                                               'markdown.extensions.toc'])
     return render(request, 'blog/detail.html', {'blog': blog})
 
