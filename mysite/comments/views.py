adfff57e4149f6b1c3db7e9766983a6282e35b42
haoran <1348195048@qq.com>
2020-06-25 17:13:17 +0800

    show comment

--- mysite/comments/views.py
+++ mysite/comments/views.py
@@ -1,6 +1,7 @@
 from blog.models import Post
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 from .forms import CommentForm
 
@@ -8,18 +9,18 @@ from .forms import CommentForm
 @require_POST
 def comment(request, post_pk):
     post = get_object_or_404(Post, pk=post_pk)
-    # django 将用户提交的数据封装在 request.POST 中，这是一个类字典对象。
-    # 我们利用这些数据构造了 CommentForm 的实例，这样就生成了一个绑定了用户提交数据的表单。
     form = CommentForm(request.POST)
 
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
+        messages.add_message(request, messages.SUCCESS, '评论发表成功！', extra_tags='success')
         return redirect(post)
 
     context = {
         'post': post,
         'form': form,
     }
+    messages.add_message(request, messages.ERROR, '评论发表失败！请修改表单中的错误后重新提交。', extra_tags='danger')
     return render(request, 'comments/preview.html', context=context)
